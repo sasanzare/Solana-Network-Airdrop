ae8756eef2cc154fb7b53a2276c8fd089777080b
sasanzare <sasanzare75@gmail.com>
2022-08-03 16:25:07 +0430

    Definition of airdrop function

--- index.js
+++ index.js
@@ -16,12 +16,23 @@ const getWalletBalance = async() =>{
         const connection = new Connection(clusterApiUrl('devnet'),'confirmed');
         const walletBalance = await connection.getBalance(publicKey);
         console.log(`Wallet balance is ${walletBalance}`);
-    } catch (error) {
-        console.error(err)
+    } catch (err) {
+        console.log(err)
     }
 }
 
+const airDropSol = async() =>{
+    try {
+        const connection = new Connection(clusterApiUrl('devnet'),'confirmed');
+        const fromAirDropSignature = await connection.requestAirdrop(publicKey, 1 * LAMPORTS_PER_SOL);
+        await connection.confirmTransaction(fromAirDropSignature);
+    } catch (err) {
+        console.log(err)
+    }
+}
 const main = async() =>{
+    await getWalletBalance();
+    await airDropSol();
     await getWalletBalance();
 }
 
